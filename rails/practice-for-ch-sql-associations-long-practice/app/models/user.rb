1dd9da85711d7d4715fb05c216bcb18cd1b4bfc6
pko912 <kopeter912@gmail.com>
2022-10-13 15:04:43 -0400

    confused

--- rails/practice-for-ch-sql-associations-long-practice/app/models/user.rb
+++ rails/practice-for-ch-sql-associations-long-practice/app/models/user.rb
@@ -8,4 +8,24 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :Enrollment
+
+         # primary_key: :id
+        # foreign_key: 
+        # class_name: 
+
+    has_many :enrolled_courses,
+        through: :enrollments,
+        source: :course
+
+
+
+
+
+       
+
+
 end
